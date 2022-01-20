525af2b4b9d47559df536bf301566bb87eaa5bde
hasanmuzak <hasanmuzak@yandex.com>
2022-01-20 16:34:18 +0300

    degisiklikler

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
+  prefix:'tw-',
   theme: {
     extend: {},
+    container: {
+      screens: {
+         sm: "100%",
+         md: "720px",
+         lg: "960px",
+         xl: "1140px",
+         '2xl' : '1320px'
+      }
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [],
-  prefix: 'tw-',
 }
